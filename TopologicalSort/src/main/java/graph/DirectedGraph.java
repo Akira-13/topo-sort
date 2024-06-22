2dfca52bf8dad6eb63333489338c73ee41c8024f
Diego Akira García Rojas <134654836+Akira-13@users.noreply.github.com>
2024-06-22 18:21:10 -0500

    Actualizado TopologicalSort y DirectedGraph
    - TopologicalSort ahroa es genérico
    - El ordenamiento topológico simple retorna una lista con los vértices ordenados
    - El ordenamiento topológico DFS asigna a cada vértice con su posición en el ordenamiento topológico
    - DirectedGraph tiene un método para retornar una lista con los vertices ordenados segun su TSNum
    - El metodo no funcionara si TSNum no ha sido definido (retornará los vértices en el orden en el que han sido agregados al grafo)

--- TopologicalSort/src/main/java/graph/DirectedGraph.java
+++ TopologicalSort/src/main/java/graph/DirectedGraph.java
@@ -1,17 +1,16 @@
 package graph;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class DirectedGraph<E> {
+
     private final Map<E, Vertex<E>> vertices;
 
     public DirectedGraph() {
         vertices = new HashMap<>();
     }
-    
-    public Vertex<E> getVertex(E id){
+
+    public Vertex<E> getVertex(E id) {
         return vertices.get(id);
     }
 
@@ -19,7 +18,7 @@ public class DirectedGraph<E> {
         vertices.putIfAbsent(data, new Vertex<>(data));
     }
 
-    public void addEdge(E fromId, E toId){
+    public void addEdge(E fromId, E toId) {
         Vertex<E> fromVertex = vertices.get(fromId);
         Vertex<E> toVertex = vertices.get(toId);
         if (fromVertex == null || toVertex == null) {
@@ -27,57 +26,48 @@ public class DirectedGraph<E> {
         }
         fromVertex.addAdjacentVertex(toVertex);
     }
-    
-    public Vertex<E> getMinimalVertex(){
-        for(Vertex<E> v : vertices.values()){
-            if(v.getExitGrade()== 0){
+
+    public Vertex<E> getMinimalVertex() {
+        for (Vertex<E> v : vertices.values()) {
+            if (v.getExitGrade() == 0) {
                 return v;
             }
         }
         return null;
     }
-    
-    public Vertex<E> vertexWithNumZero(){
-        for(Vertex<E> v : vertices.values()){
-            if(v.getNum() == 0){
+
+    public Vertex<E> vertexWithNumZero() {
+        for (Vertex<E> v : vertices.values()) {
+            if (v.getNum() == 0) {
                 return v;
             }
         }
         return null;
     }
-    
-    public void removeVertex(E data){
+
+    public void removeVertex(E data) {
         Vertex<E> remVertex = vertices.remove(data);
-        for(Vertex<E> v : vertices.values()){
+        for (Vertex<E> v : vertices.values()) {
             v.removeAdjacentVertex(remVertex);
         }
     }
-    
-    public Collection<Vertex<E>> getAllVertices(){
+
+    public Collection<Vertex<E>> getAllVertices() {
         return vertices.values();
     }
 
     @Override
     public String toString() {
         StringBuilder string = new StringBuilder();
-        for(Vertex v : vertices.values()){
+        for (Vertex v : vertices.values()) {
             string.append(v.toString()).append("\n");
         }
         return string.toString();
     }
-    
-    public String topologicalOrdering(){
-        StringBuilder string = new StringBuilder();
-        for(int i = 1; i <= vertices.size(); i++){
-            for(Vertex<E> v : vertices.values()){
-                if(i == v.getTSnum()){
-                    string.append(v.getId()).append(" ");
-                }
-            }
-            if(string.length() != 2*i){
-                return "Grafo no ordenado";
-            }
-        }
-        return string.toString();
+
+    public List<Vertex<E>> getTopologicalOrdering() {
+        List<Vertex<E>> sortedVertices = new ArrayList<>(vertices.values());
+        sortedVertices.sort(Comparator.comparingInt(Vertex::getTSnum));
+        return sortedVertices;
     }
 }
--- TopologicalSort/src/main/java/graph/TopologicalSort.java
+++ TopologicalSort/src/main/java/graph/TopologicalSort.java
@@ -3,38 +3,47 @@ package graph;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TopologicalSort {
+public class TopologicalSort<E> {
 
-    private int i = 1;
-    private int j = 1;
-
-    public List<Vertex> topologicalSort(DirectedGraph graph) {
-        ArrayList<Vertex> sortedVertices = new ArrayList<>();
+    private int i;
+    private int j;
+    
+    /*
+    //  topologicalSort sin DFS   //
+    Drozdek no especifica tipo de retorno.
+    La función retorna una lista con los vértices ordenados
+    empezando desde el primer vértice mínimo que se encuentre.
+    */
+    public List<Vertex<E>> topologicalSort(DirectedGraph<E> graph) {
+        ArrayList<Vertex<E>> sortedVertices = new ArrayList<>();
         int size = graph.getAllVertices().size();
         for (int r = 1; r <= size; r++) {
-            Vertex v = graph.getMinimalVertex();
+            Vertex<E> v = graph.getMinimalVertex();
             if (v == null) {
                 throw new IllegalArgumentException("Grafo con ciclo");
             }
+            /*
+            Actualiza ambos TSnum y num de Vertex
+            */
+            v.setTSnum(r);
             v.setNum(r);
-            graph.removeVertex(v.getId());
+            graph.removeVertex((E) v.getId());
             sortedVertices.add(v);
             //System.out.println(graph.toString());
         }
         return sortedVertices;
     }
-
-    public void topologicalSorting(DirectedGraph graph) {
-        while (graph.vertexWithNumZero() != null) {
-            TS(graph.vertexWithNumZero());
-        }
-        System.out.println(graph.topologicalOrdering());
-        i = j = 1;
-    }
-
-    private void TS(Vertex v) {
+    
+    /*
+    //  topologicalSort con DFS   //
+    Drozdek sugiere imprimir los vertices después del ordenamiento.
+    Se delega esta funcion a un metodo en la clase DirectedGraph para
+    evitar que el algoritmo haga más de lo esperado.
+    Se asigna un numero de orden a los vertices.
+    */
+    private void TS(Vertex<E> v) {
         v.setNum(i++);
-        for(Vertex adjacentVertex : (List<Vertex>)  v.getEdges()){
+        for(Vertex<E> adjacentVertex : v.getEdges()){
             if(adjacentVertex.getNum() == 0){
                 TS(adjacentVertex);
             }
@@ -43,5 +52,19 @@ public class TopologicalSort {
             }
         }
         v.setTSnum(j++);
+    }
+    
+    public void topologicalSortDFS(DirectedGraph<E> graph) {
+        i = j = 0;
+        while (graph.vertexWithNumZero() != null) {
+            TS(graph.vertexWithNumZero());
+        }
+        i = j = 0;
+        /*
+        List<Vertex<E>> orderedVertices = graph.getTopologicalOrdering();
+        for(Vertex<E> v : orderedVertices){
+            System.out.print(v.toString());
+        }
+        */
     }
 }
