c549335f0c1677e562e3a1595ab969222409c980
camila <camila@CamilaLopez>
2024-06-28 08:53:18 -0500

    Comentado TDA PlanificadorCursos

--- TopologicalSort/src/main/java/planificadorcursos/Curso.java
+++ TopologicalSort/src/main/java/planificadorcursos/Curso.java
@@ -7,9 +7,14 @@ package planificadorcursos;
 import graph.Vertex;
 
 /**
- *
- * @author camila
+ * La clase Curso representa un curso individual en el contexto de un planificador de cursos.
+ * Extiende la clase Vertex<String>, lo que significa que cada curso es un vértice en un grafo dirigido,
+ * donde el identificador del curso es un String. Además, la clase incluye un atributo booleano para 
+ * indicar si el curso ha sido completado.
+ * 
+ * @autor camila
  */
+
 public class Curso extends Vertex<String> {
     private boolean completado;
     
@@ -20,11 +25,20 @@ public class Curso extends Vertex<String> {
     }
     
     
-    
+    /**
+     * Método para verificar si el curso ha sido completado.
+     * 
+     * @return true si el curso ha sido completado, false en caso contrario.
+     */
     public boolean isCompletado() {
         return completado;
     }
-
+    
+    /**
+     * Método para establecer el estado de completado del curso.
+     * 
+     * 
+     */
     public void setCompletado(boolean completado) {
         this.completado = completado;
     }
